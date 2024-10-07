40d11815662ae1b5377387c0add72cd013f38d33
galangriarulhaq <galangrz337@gmail.com>
2024-10-08 01:36:38 +0700

    fix minor errors

--- app/src/main/java/com/bangkit/eventdicodingapp/data/retrofit/ApiService.kt
+++ app/src/main/java/com/bangkit/eventdicodingapp/data/retrofit/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("events?active=1")
+    @GET("events?active=0")
     fun getEventUpcoming(): Call<EventResponse>
 
 }
