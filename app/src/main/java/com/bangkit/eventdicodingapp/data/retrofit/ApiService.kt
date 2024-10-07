d5135e0f91327dc6a19272e265e8251226497753
galangriarulhaq <galangrz337@gmail.com>
2024-10-08 00:05:22 +0700

    Create ApiConfig

--- app/src/main/java/com/bangkit/eventdicodingapp/data/retrofit/ApiService.kt
+++ app/src/main/java/com/bangkit/eventdicodingapp/data/retrofit/ApiService.kt
@@ -1,4 +1,11 @@
 package com.bangkit.eventdicodingapp.data.retrofit
 
+import com.bangkit.eventdicodingapp.data.response.EventResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("events?active=1")
+    fun getEventUpcoming(): Call<EventResponse>
+
 }
